b62346fec326f971a267b8cf4628e619cd6123c0
Giovanni Carillo <giovicarillo@gmail.com>
2026-02-28 15:57:03 +0100

    rimosso file inutili da github

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 To make it work you have to get your API key from https://openweathermap.org/
 and put it in CHIAVE_API
+Go to https://aistudio.google.com/api-keys and put your API key in CHIAVE_GEMINI
--- app.py
+++ app.py
@@ -2,32 +2,49 @@ import streamlit as st
 import requests
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 # Nota: Su Streamlit Cloud leggeremo la chiave in modo diverso (punto 4),
 # ma questo codice va bene per entrambi.
 api_key = os.getenv("API_KEY")
+CHIAVE_GEMINI= os.getenv("GEMINI_KEY")
+
+client= genai.Client(api_key=CHIAVE_GEMINI)
+
+
+def chiamata(city, temp, desc):
+    prompt=f"Sei un pappagallo simpatico. A {city} ci sono {temp} gradi e il tempo è {desc}. Commenta brevemente il tempo usando anche l'emoji del pappagallo"
+    risposta=client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+    return risposta.text
 
 st.set_page_config(page_title="Meteo App", page_icon="🌤️")
 
-st.title("🌦️ La mia prima App Meteo")
+st.title("🌦️Il meteo del pappagallo🦜")
 st.write("Inserisci il nome di una città per sapere che tempo fa.")
 
 citta = st.text_input("Città", placeholder="Es: Roma")
 
-if st.button("Controlla Meteo"):
+if st.button("Controlla Meteo") or citta:
     if not api_key:
         st.error("Errore: Chiave API non trovata!")
     elif citta:
-        url = f"http://api.openweathermap.org/data/2.5/weather?q={citta}&appid={api_key}&units=metric&lang=it"
-        res = requests.get(url)
-        if res.status_code == 200:
-            dati = res.json()
-            temp = dati["main"]["temp"]
-            desc = dati["weather"][0]["description"]
-            st.metric(label="Temperatura", value=f"{temp} °C")
-            st.info(f"Condizioni: {desc.capitalize()}")
-        else:
-            st.error("Città non trovata. Riprova!")
+        with st.status(f"Volando verso {citta}...", expanded=True) as status:
+            url = f"http://api.openweathermap.org/data/2.5/weather?q={citta}&appid={api_key}&units=metric&lang=it"
+            res = requests.get(url)
+            if res.status_code == 200:
+                dati = res.json()
+                temp = dati["main"]["temp"]
+                desc = dati["weather"][0]["description"]
+                st.metric(label="Temperatura", value=f"{temp} °C")
+                st.info(f"Condizioni: {desc.capitalize()}")
+                st.write("Il pappagallo sta per dare la sua risposta...")
+                ans_gem=chiamata(citta, temp, desc)
+                status.update(label="Risposta pronta!", state="complete")
+
+            else:
+                st.error("Città non trovata. Riprova!")
+        with st.chat_message("assistant", avatar="🦜"):
+            st.write(ans_gem)
     else:
         st.warning("Per favore, inserisci il nome di una città.")
